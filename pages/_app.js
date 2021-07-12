ee8a07fe30273be7f2ceeea23d736d015340a391
TimGuo8 <gth991207@gmail.com>
2021-07-12 15:26:32 -0700

    Get rid of Bulma files

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }) {
       <div>
         <Head>
          <title>WebGPU-LitmusTest</title>
-         
+         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css"></link>
         </Head>
       <div className="columns">
         <div className="column is-one-quarter">
