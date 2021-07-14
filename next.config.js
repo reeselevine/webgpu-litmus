68f1ab29f45eb2a24d40590c25f9cca21c59936c
Reese Levine <reeselevine1@gmail.com>
2021-07-14 16:52:09 -0700

    Clean up site, set up static hosting

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+    }
+  }
+
+  return {
+    assetPrefix: '/~reeselevine/webgpu/',
+    basePath: '/~reeselevine/webgpu'
+  }
+}
