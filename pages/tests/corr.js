8c82834a2b73c82624d9c0682acc585b9ca07d2a
Reese Levine <reeselevine1@gmail.com>
2021-07-15 20:27:48 -0700

    Add corr test

--- pages/tests/corr.js
+++ pages/tests/corr.js
@@ -6,106 +6,106 @@ import * as ReactBootStrap from 'react-bootstrap';
 
 const shaderCode = `
 [[block]] struct AtomicMemory {
-  value: array<atomic<u32>>;
-};
-[[block]] struct Memory {
-  value: array<u32>;
-};
-
-[[group(0), binding(0)]] var<storage, read_write> test_data : AtomicMemory;
-[[group(0), binding(1)]] var<storage, read_write> mem_locations : Memory;
-[[group(0), binding(2)]] var<storage, read_write> results : AtomicMemory;
-[[group(0), binding(3)]] var<storage, read_write> shuffled_ids : Memory;
-[[group(0), binding(4)]] var<storage, read_write> barrier : AtomicMemory;
-[[group(0), binding(5)]] var<storage, read_write> scratchpad : Memory;
-[[group(0), binding(6)]] var<storage, read_write> scratch_locations : Memory;
-[[group(0), binding(7)]] var<storage, read_write> stress_params : Memory;
-
-fn spin() {
-  var i : u32 = 0u;
-  var bar_val : u32 = atomicAdd(&barrier.value[0], 1u);
-  loop {
-    if (i == 1024u || bar_val >= 2u) {
-      break;
-    }
-    bar_val = atomicLoad(&barrier.value[0]);
-    i = i + 1u;
-  }
-}
-
-fn do_stress(iterations: u32, pattern: u32, workgroup_id: u32) {
-  switch(pattern) {
-    case 0u: {
-      for(var i: u32 = 0u; i < iterations; i = i + 1u) {
-        scratchpad.value[scratch_locations.value[workgroup_id]] = i;
-        scratchpad.value[scratch_locations.value[workgroup_id]] = i + 1u;
+    value: array<atomic<u32>>;
+  };
+  [[block]] struct Memory {
+    value: array<u32>;
+  };
+  
+  [[group(0), binding(0)]] var<storage, read_write> test_data : AtomicMemory;
+  [[group(0), binding(1)]] var<storage, read_write> mem_locations : Memory;
+  [[group(0), binding(2)]] var<storage, read_write> results : AtomicMemory;
+  [[group(0), binding(3)]] var<storage, read_write> shuffled_ids : Memory;
+  [[group(0), binding(4)]] var<storage, read_write> barrier : AtomicMemory;
+  [[group(0), binding(5)]] var<storage, read_write> scratchpad : Memory;
+  [[group(0), binding(6)]] var<storage, read_write> scratch_locations : Memory;
+  [[group(0), binding(7)]] var<storage, read_write> stress_params : Memory;
+  
+  fn spin() {
+    var i : u32 = 0u;
+    var bar_val : u32 = atomicAdd(&barrier.value[0], 1u);
+    loop {
+      if (i == 1024u || bar_val >= 2u) {
+        break;
       }
-    }
-    case 1u: {
-      for(var i: u32 = 0u; i < iterations; i = i + 1u) {
-        scratchpad.value[scratch_locations.value[workgroup_id]] = i;
-        let tmp1: u32 = scratchpad.value[scratch_locations.value[workgroup_id]];
-        if (tmp1 > 100u) {
-          break;
-        }
-      }
-    }
-    case 2u: {
-      for(var i: u32 = 0u; i < iterations; i = i + 1u) {
-        let tmp1: u32 = scratchpad.value[scratch_locations.value[workgroup_id]];
-        if (tmp1 > 100u) {
-          break;
-        }
-        scratchpad.value[scratch_locations.value[workgroup_id]] = i;
-      }
-    }
-    case 3u: {
-      for(var i: u32 = 0u; i < iterations; i = i + 1u) {
-        let tmp1: u32 = scratchpad.value[scratch_locations.value[workgroup_id]];
-        if (tmp1 > 100u) {
-          break;
-        }
-        let tmp2: u32 = scratchpad.value[scratch_locations.value[workgroup_id]];
-        if (tmp2 > 100u) {
-          break;
-        }
-      }
-    }
-    default: {
-      break;
+      bar_val = atomicLoad(&barrier.value[0]);
+      i = i + 1u;
     }
   }
-}
-
-let workgroupXSize = 1;
-[[stage(compute), workgroup_size(workgroupXSize)]] fn main([[builtin(workgroup_id)]] workgroup_id : vec3<u32>, [[builtin(global_invocation_id)]] global_invocation_id : vec3<u32>, [[builtin(local_invocation_index)]] local_invocation_index : u32) {
-  var y : u32 = mem_locations.value[0];
-  var x : u32 = mem_locations.value[1];
-  if (shuffled_ids.value[global_invocation_id[0]] == u32(workgroupXSize) * 0u + 0u) {
-    if (stress_params.value[4] == 1u) {
-      do_stress(stress_params.value[5], stress_params.value[6], workgroup_id[0]);
+  
+  fn do_stress(iterations: u32, pattern: u32, workgroup_id: u32) {
+    switch(pattern) {
+      case 0u: {
+        for(var i: u32 = 0u; i < iterations; i = i + 1u) {
+          scratchpad.value[scratch_locations.value[workgroup_id]] = i;
+          scratchpad.value[scratch_locations.value[workgroup_id]] = i + 1u;
+        }
+      }
+      case 1u: {
+        for(var i: u32 = 0u; i < iterations; i = i + 1u) {
+          scratchpad.value[scratch_locations.value[workgroup_id]] = i;
+          let tmp1: u32 = scratchpad.value[scratch_locations.value[workgroup_id]];
+          if (tmp1 > 100u) {
+            break;
+          }
+        }
+      }
+      case 2u: {
+        for(var i: u32 = 0u; i < iterations; i = i + 1u) {
+          let tmp1: u32 = scratchpad.value[scratch_locations.value[workgroup_id]];
+          if (tmp1 > 100u) {
+            break;
+          }
+          scratchpad.value[scratch_locations.value[workgroup_id]] = i;
+        }
+      }
+      case 3u: {
+        for(var i: u32 = 0u; i < iterations; i = i + 1u) {
+          let tmp1: u32 = scratchpad.value[scratch_locations.value[workgroup_id]];
+          if (tmp1 > 100u) {
+            break;
+          }
+          let tmp2: u32 = scratchpad.value[scratch_locations.value[workgroup_id]];
+          if (tmp2 > 100u) {
+            break;
+          }
+        }
+      }
+      default: {
+        break;
+      }
     }
-    if (stress_params.value[0] == 1u) {
-      spin();
-    }
-    atomicStore(&test_data.value[y], 1u);
-    atomicStore(&test_data.value[x], 1u);
-  } elseif (shuffled_ids.value[global_invocation_id[0]] == u32(workgroupXSize) * 1u + 0u) {
-    if (stress_params.value[4] == 1u) {
-      do_stress(stress_params.value[5], stress_params.value[6], workgroup_id[0]);
-    }
-    if (stress_params.value[0] == 1u) {
-      spin();
-    }
-    let r0 = atomicLoad(&test_data.value[x]);
-    let r1 = atomicLoad(&test_data.value[y]);
-    atomicStore(&results.value[1], r1);
-    atomicStore(&results.value[0], r0);
-  } elseif (stress_params.value[1] == 1u) {
-    do_stress(stress_params.value[2], stress_params.value[3], workgroup_id[0]);
   }
-}
+  
+  let workgroupXSize = 1;
+  [[stage(compute), workgroup_size(workgroupXSize)]] fn main([[builtin(workgroup_id)]] workgroup_id : vec3<u32>, [[builtin(global_invocation_id)]] global_invocation_id : vec3<u32>, [[builtin(local_invocation_index)]] local_invocation_index : u32) {
+    var x : u32 = mem_locations.value[0];
+    var y : u32 = mem_locations.value[1];
+    if (shuffled_ids.value[global_invocation_id[0]] == u32(workgroupXSize) * 0u + 0u) {
+      if (stress_params.value[4] == 1u) {
+        do_stress(stress_params.value[5], stress_params.value[6], workgroup_id[0]);
+      }
+      if (stress_params.value[0] == 1u) {
+        spin();
+      }
+      atomicStore(&test_data.value[x], 1u);
+    } elseif (shuffled_ids.value[global_invocation_id[0]] == u32(workgroupXSize) * 1u + 0u) {
+      if (stress_params.value[4] == 1u) {
+        do_stress(stress_params.value[5], stress_params.value[6], workgroup_id[0]);
+      }
+      if (stress_params.value[0] == 1u) {
+        spin();
+      }
+      let r0 = atomicLoad(&test_data.value[x]);
+      let r1 = atomicLoad(&test_data.value[y]);
+      atomicStore(&results.value[0], r0);
+      atomicStore(&results.value[1], r1);
+    } elseif (stress_params.value[1] == 1u) {
+      do_stress(stress_params.value[2], stress_params.value[3], workgroup_id[0]);
+    }
+  }
 `
+
 
 function buildThrottle(updateFunc) {
   const throttled = _.throttle((newValue) => updateFunc(newValue), 50);
@@ -128,7 +128,10 @@ function handleResult(result, state) {
   }
 }
 
-export default function MessagePassing() {
+export default function CoRR() {
+  const testParams = JSON.parse(JSON.stringify(defaultTestParams));
+  testParams.memoryAliases[1] = 0;
+
   const [bothOne, setBothOne] = useState(0);
   const [oneZero, setOneZero] = useState(0);
   const [zeroOne, setZeroOne] = useState(0);
@@ -160,7 +163,7 @@ export default function MessagePassing() {
     }
   }
 
-  function doMessagePassing() {
+  function doCoRR() {
     setLoading(true);
     state.bothZero.value = 0;
     state.bothZero.update(0);
@@ -170,7 +173,7 @@ export default function MessagePassing() {
     state.oneZero.update(0);
     state.zeroOne.value = 0;
     state.zeroOne.update(0);
-    const p = runLitmusTest(shaderCode, defaultTestParams, iterations, handleResult, state);
+    const p = runLitmusTest(shaderCode, testParams, iterations, handleResult, state);
     p.then(
       success => {
         setLoading(false);
@@ -205,8 +208,8 @@ export default function MessagePassing() {
   <>
     <div className="columns">
       <div className="column">
-        <h1 className="testName">Message Passing</h1>
-          <h2 className="testDescription">The message passing litmus test checks to see if two stores in one thread can be re-ordered according to loads on a second thread.</h2>
+        <h1 className="testName">CoRR</h1>
+          <h2 className="testDescription">The CoRR litmus test checks to see if memory is coherent.</h2>
       </div>
     </div>
     <div className=" columns">
@@ -329,7 +332,7 @@ export default function MessagePassing() {
     </div>
     <div className="buttons mt-2">
       <button className="button is-primary" onClick={()=>{
-              doMessagePassing();
+              doCoRR();
             }} disabled={iterations < 0}>Start Test</button>
     </div>
     </div>
