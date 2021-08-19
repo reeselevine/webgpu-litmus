14a7a768224845bfbf7012ffbf5bd341fd4a86cf
Reese Levine <reeselevine1@gmail.com>
2021-08-19 10:55:48 -0700

    Add more mp variants

--- pages/tests/message-passing.js
+++ pages/tests/message-passing.js
@@ -3,21 +3,49 @@ import { commonHandlers, makeTwoOutputLitmusTestPage } from '../../components/te
 import {TestSetupPseudoCode, buildPseudoCode} from '../../components/testPseudoCode.js'
 import messagePassing from '../../shaders/message-passing.wgsl'
 import barrierMessagePassing from '../../shaders/barrier-message-passing.wgsl'
+import barrier1MessagePassing from '../../shaders/barrier1-message-passing.wgsl'
+import barrier2MessagePassing from '../../shaders/barrier2-message-passing.wgsl'
+import barrierMessagePassingNA from '../../shaders/barrier-message-passing-na.wgsl';
+
+const thread0B = `0.1: store(x, 1)
+0.2: barrier()
+0.3: store(y, 1)`;
+
+const thread1B = `1.1: r0=load(y)
+1.2: barrier()
+1.3: r1=load(x)`;
+
+const thread0NB = `0.1: store(x, 1)
+0.2: store(y, 1)`
+
+const thread1NB = `1.1: r0=load(y)
+1.2: r1=load(x)`
 
 const variants = {
   default: {
-    pseudo: buildPseudoCode([`0.1: x=1
-0.2: y=1`, `1.1: r0=y
-1.2: r1=x`]),
+    pseudo: buildPseudoCode([thread0NB, thread1NB]),
     shader: messagePassing
   },
   barrier: {
-    pseudo: buildPseudoCode([`0.1: store(x, 1)
+    pseudo: buildPseudoCode([thread0B, thread1B]),
+    shader: barrierMessagePassing
+  },
+  barrier1: {
+    pseudo: buildPseudoCode([thread0B, thread1NB]),
+    shader: barrier1MessagePassing
+  },
+  barrier2: {
+    pseudo: buildPseudoCode([thread0NB, thread1B]),
+    shader: barrier2MessagePassing
+  },
+  nonatomic: {
+    pseudo: buildPseudoCode([`0.1: x=1
 0.2: barrier()
 0.3: store(y, 1)`, `1.1: r0=load(y)
 1.2: barrier()
-1.3: r1=load(x)`]),
-    shader: barrierMessagePassing
+1.3: if (r0 == 1):
+1.4:   r1=x`]),
+    shader: barrierMessagePassingNA 
   }
 }
 
