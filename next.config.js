82efd4f2aa548a3fd0381f6716db4a8e024c4b21
Reese Levine <reeselevine1@gmail.com>
2022-12-13 17:44:18 -0800

    Change config up a little

--- next.config.js
+++ next.config.js
@@ -1,35 +1,15 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
-
-module.exports = (phase, { defaultConfig }) => {
-  const loader = {
+module.exports = {
+    assetPrefix: process.env.ASSET_PREFIX,
+    basePath: process.env.BASE_PATH,
+    env: {
+      corsAllow: process.env.CORS_ALLOW,
+        dataApi: process.env.DATA_API
+    },
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
       config.module.rules.push({
         test: /\.wgsl$/i,
         loader: "raw-loader",
       });
-
       return config;
     }
-  };
-
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      ...loader,
-      env: {
-        corsAllow: "http://localhost:3000",
-        dataApi: "http://localhost:3000/api"
-      }
-    }
-  }
-
-  return {
-    assetPrefix: '/webgpu-mem-testing',
-    basePath: '/webgpu-mem-testing',
-    trailingSlash: true,
-    env: {
-      corsAllow: "https://gpuharbor.ucsc.edu/webgpu-mem-testing/",
-        dataApi: "http://localhost:3000/api"
-    },
-    ...loader
-  }
 }
