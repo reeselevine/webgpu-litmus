406c779fdcf44117d9330e65f8637e22d84248cb
Reese Levine <reeselevine1@gmail.com>
2022-12-19 20:24:34 -0800

    move to sqlite

--- components/db-connector.js
+++ components/db-connector.js
@@ -1,43 +1,21 @@
-import mariadb from 'mariadb'
+import Database from 'better-sqlite3'
 
-class MariaDBConnector {
+const DB_PATH = "./gpuharbor.db";
+
+class SqliteConnector {
 
   constructor() {
-    this.pool = mariadb.createPool({
-      host: 'localhost', 
-      user:'reeselevine', 
-      password: '',
-      connectionLimit: 5
-    });
+    this.db = new Database(DB_PATH);
+    const setup = this.db.prepare(`
+      create table if not exists tuning_results (
+        name text,
+        email text,
+        results text
+      );`);
+    const info = setup.run();
+    console.log(info.changes);
   }
 
-  async setup() {
-    let conn;
-    try {
-	    conn = await this.pool.getConnection();
-      let res = await conn.query("CREATE DATABASE IF NOT EXISTS gpuharbor");
-      console.log(res);
-      res = await conn.query(`
-        CREATE TABLE IF NOT EXISTS gpuharbor.tuningResults (
-          id int AUTO_INCREMENT,
-          name varchar(255),
-          email varchar(255),
-          results JSON,
-          CHECK (JSON_VALID(results)),
-          PRIMARY KEY (id)
-        )`);
-        console.log(res);
-    } catch (err) {
-      throw err;
-    } finally {
-	    if (conn) return conn.end();
-    }
-  }
 }
 
-export const databaseConnector = new MariaDBConnector();
-try {
-  const res = await databaseConnector.setup();
-} catch (err) {
-  //do nothing for now
-}
+export const databaseConnector = new SqliteConnector();
