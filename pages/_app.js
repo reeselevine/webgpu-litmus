ef1626a2999055c40f66d6b37df9250177e21a1d
Reese Levine <reeselevine1@gmail.com>
2023-10-29 14:24:56 -0700

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import '../sass/style.css'
 import Head from 'next/head'
 import Panel from '../components/panel'
 import Link from 'next/link'
+import Script from 'next/script'
+
 export default function App({ Component, pageProps }) {
   return (
     <div>
@@ -11,6 +13,18 @@ export default function App({ Component, pageProps }) {
         <link rel="stylesheet" href="https://www.jsdelivr.com/package/npm/bulma"></link>
         <meta name="originToken" httpEquiv="origin-trial" content="Ah4uWU+/egyrlov7rmCK2NGM3x2rhtHGK7E5WWtmq3ESA+JdCBbr29qZkE2iYWZBZJ72McNs+9cJ8PcIKaI67gAAAABSeyJvcmlnaW4iOiJodHRwczovL2dwdWhhcmJvci51Y3NjLmVkdTo0NDMiLCJmZWF0dXJlIjoiV2ViR1BVIiwiZXhwaXJ5IjoxNjkxNzExOTk5fQ=="></meta>
       </Head>
+      <div className="container">
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-9SHY5MMRR1" />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-9SHY5MMRR1');
+        `}
+        </Script>
+      </div>
       <div className="columns">
         <div className="column is-one-fifth">
           <Panel>
